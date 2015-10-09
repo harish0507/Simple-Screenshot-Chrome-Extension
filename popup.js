c1bfdd29edd9f717411e69a461a8bdc89831db47
Harish <harishprofile001@gmail.com>
2015-10-09 17:03:15 +0530

    initial commit

---
+++ popup.js
@@ -0,0 +1,41 @@
+function takeScreenshot() {
+  
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    renderStatus('Taking screenshot of ' + url);
+    setTimeout(function() {
+      chrome.tabs.captureVisibleTab( 
+        chrome.windows.WINDOW_ID_CURRENT,
+        function(src) {
+          var a = document.getElementById("save");
+          a.href = src;
+          a.target = "_blank";
+          var img = document.getElementById("screenshot");
+          img.title = "Screenshot Image";
+          img.src = src;
+          renderStatus('Screenshot of ' + url);
+          img.hidden = false;
+        }
+      );
+    }, 1000);
+  });
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+function closePopup() {
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  takeScreenshot();
+  document.getElementById('close').addEventListener('click', closePopup);
+});
